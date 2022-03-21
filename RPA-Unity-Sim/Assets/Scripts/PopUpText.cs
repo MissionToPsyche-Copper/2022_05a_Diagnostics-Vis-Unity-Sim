4c466963e4943108566dffa34f3bae6eb099a136
TyeDye220 <72174974+TyeDye220@users.noreply.github.com>
2022-03-21 16:44:36 -0700

    General Fixes
    Fixed some spelling/grammar issues with the rpa tutorial. Fixed a weird thing where the main camera position wasn't set in the editor.

--- RPA-Unity-Sim/Assets/Scripts/PopUpText.cs
+++ RPA-Unity-Sim/Assets/Scripts/PopUpText.cs
@@ -22,6 +22,15 @@ public class PopUpText : MonoBehaviour
     private void OnMouseOver()
     {
         string text = tag;
+        switch(text)
+        {
+            case "Floating Grid": text += "\nOnly allows Xenon ions and electrons into the device"; break;
+            case "Electron Repulsion Grid": text += "\nStops electrons and allows ions to pass through"; break;
+            case "Ion Retarding Grid": text += "\nOnly allows ions with a high enough energy to pass through"; break;
+            case "Electron Suppression Grid": text += "\nForces electrons removed from the collector to return to it"; break;
+            case "Collector": text += "\nCounts the number of ions that reach it"; break;
+            default: text += ""; break;
+        }
         textBox.text = text;
     }
 
